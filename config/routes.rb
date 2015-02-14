e4bf62f5ca2441b4af9a4c760be822f3a89b4b75
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-14 16:18:01 +1300

    Change root route to home

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
 
-  root 'application#hello'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
