898ba642dde6e39301cfcb7fb4a74fac8a480a6d
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-24 07:11:01 +1300

    Add log_in method to SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+    # The session method provided by Rails is distinct from the SessionsController.
+    # We can treat session as a hash. It represents a temporary cookie on the user's
+    # browser containing an encrypted version of the user's id. This allows us to
+    # retrieve the id on subsequent pages using session[:user_id]
+    # In contrast to the persistent cookie created by the cookies method, this
+    # cookie expires when the browser is closed.
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
 end
