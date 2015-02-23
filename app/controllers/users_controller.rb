530d282f7e1ac25c1a544d44549c86a42a9a006f
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-24 12:24:04 +1300

    Add login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user    # equivalent to redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
@@ -21,7 +22,8 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
 end
