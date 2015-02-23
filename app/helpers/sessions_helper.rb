1677838338e699a17fca396258ac021545797341
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-24 12:05:40 +1300

    Add logged_in? method to SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,8 +11,14 @@ module SessionsHelper
     # cookie expires when the browser is closed.
   end
 
+  # Returns the currently logged-in user 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
   end
 
 end
