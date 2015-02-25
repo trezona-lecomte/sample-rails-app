6033f308408677f61e610796292cf227a845fbb8
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-25 23:49:22 +1300

    Use redirect_back_or in create of SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user   # Calls log_in of SessionsHelper - stores user_id in session[]
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
