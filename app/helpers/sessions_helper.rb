f04258851b965bba651352ddb729fb6941beec06
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-25 19:48:44 +1300

    Add logout functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,13 +3,13 @@ module SessionsHelper
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
-    # The session method provided by Rails is distinct from the SessionsController.
-    # We can treat session as a hash. It represents a temporary cookie on the user's
-    # browser containing an encrypted version of the user's id. This allows us to
-    # retrieve the id on subsequent pages using session[:user_id]
-    # In contrast to the persistent cookie created by the cookies method, this
-    # cookie expires when the browser is closed.
   end
+  # The session method provided by Rails is distinct from the SessionsController.
+  # We can treat session as a hash. It represents a temporary cookie on the user's
+  # browser containing an encrypted version of the user's id. This allows us to
+  # retrieve the id on subsequent pages using session[:user_id]
+  # In contrast to the persistent cookie created by the cookies method, this
+  # cookie expires when the browser is closed.
 
   # Returns the currently logged-in user 
   def current_user
@@ -19,6 +19,12 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,11 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
+
+  # Returns true if a test user is logged in.
+  # This method parallels the logged_in? helper method in SessionsH
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
 end
