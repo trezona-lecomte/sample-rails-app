e1f6bf4bf8706f4d71bac76e5ae7780a33a0dbcd
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-26 22:46:40 +1300

    Test that forwarding url only redirects once after login

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -30,6 +30,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "" }
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_nil session[:forwarding_url]
     @user.reload
     assert_equal @user.name, name
     assert_equal @user.email, email
