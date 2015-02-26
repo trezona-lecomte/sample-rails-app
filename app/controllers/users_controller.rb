2822da9d5a28337d5a373e20e775431247700f72
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-26 20:07:50 +1300

    Allow viewing of index page when logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    
+  end
 
   def show
     @user = User.find(params[:id])
@@ -37,6 +41,7 @@ class UsersController < ApplicationController
 
   private
 
+    # Enforce strong parameters for creating/editing users.
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
