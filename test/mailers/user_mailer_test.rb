053a8acdfd7556eeec8c02f324f7dbb4541c5b77
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-28 11:46:31 +1300

    Configure mailer preview for account activation

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.account_activation
     assert_equal "Account activation", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
@@ -13,7 +13,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
