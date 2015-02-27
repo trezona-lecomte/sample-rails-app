846e85ee3e92c08bd67dbc9f308e6f9904f037b5
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-28 12:24:11 +1300

    Fix bug in account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,7 +4,7 @@ def edit
   user = User.find_by(email: params[:email])
   if user && !user.activated? && user.authenticated?(:activation, params[:id])
     user.update_attribute(:activated, true)
-    user.update_attribute(:activate_at, Time.zone.now)
+    user.update_attribute(:activated_at, Time.zone.now)
     log_in user
     flash[:success] = "Account activated"
     redirect_to user
