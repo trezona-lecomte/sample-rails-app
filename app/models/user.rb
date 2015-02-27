09d96dbe4d1ff2607c861ad9560ba7d51a9b2884
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-27 22:52:53 +1300

    Refactor downcasing of email in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token
-  before_save { email.downcase! }
+  before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -46,6 +46,10 @@ class User < ActiveRecord::Base
   end
 
   private
+
+    def downcase_email
+      self.email = email.downcase
+    end
 
     def create_activation_digest
       self.activation_token = User.new_token
