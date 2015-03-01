eea007d9db0442be698d9d036e973d8528f81668
trezona-lecomte <trezona.lecomte@gmail.com>
2015-03-01 22:50:50 +1300

    Add create and destroy actions to microposts controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,25 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    # Enforce strong parameters for creating/editing users.
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+
 end
