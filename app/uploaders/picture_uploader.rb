01d29c8c2c3faa89980449919095dc3774de3234
trezona-lecomte <trezona.lecomte@gmail.com>
2015-03-04 15:13:16 +1300

    Add limit of 400x400 to uploaded images

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -21,7 +22,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
