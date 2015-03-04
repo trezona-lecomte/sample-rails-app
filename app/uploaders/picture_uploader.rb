941f113560ce6ed265ee5d05db6b86cb4707bc9b
trezona-lecomte <trezona.lecomte@gmail.com>
2015-03-04 14:25:13 +1300

    Enable picture file type validation

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,6 +16,12 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -34,12 +40,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
   # end
 
   # Override the filename of the uploaded files:
